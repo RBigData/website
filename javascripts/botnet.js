c98f29d43521dcfe0ba46f9e05d1e7a72a49f15b
Drew Schmidt <schmidt@math.utk.edu>
2013-05-06 00:57:53 -0400

    05.06.13 nightly

--- javascripts/botnet.js
+++ javascripts/botnet.js
@@ -1,5 +1,3 @@
-<script type="text/javascript">
-
   var _gaq = _gaq || [];
   _gaq.push(['_setAccount', 'UA-40654684-1']);
   _gaq.push(['_setDomainName', 'r-pbd.org']);
@@ -10,5 +8,3 @@
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
-
-</script>
