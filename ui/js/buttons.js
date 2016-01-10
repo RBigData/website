d9801e59bce6a35767b89d07938ab8ed6b9d551f
Drew Schmidt <schmidt@math.utk.edu>
2016-01-10 16:52:25 -0500

    trying without jquery...

--- ui/js/buttons.js
+++ ui/js/buttons.js
@@ -1,3 +1,3 @@
-var rghb = function(){$("body").html($("body").html().replace(/ghbutton/g,'<span class="label label-default" style="background-color:green">GitHub</span>'));return;}; rghb();
-var rcb = function(){$("body").html($("body").html().replace(/cranbutton/g,'<span class="label label-default" style="background-color:blue">CRAN</span>'));return;}; rcb();
+document.body.innerHTML = document.body.innerHTML.replace(/ghbutton/g, '<span class="label label-default" style="background-color:green">GitHub</span>');
+document.body.innerHTML = document.body.innerHTML.replace(/cranbutton/g, '<span class="label label-default" style="background-color:blue">CRAN</span>');
 
