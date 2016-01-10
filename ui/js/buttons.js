aa1882d74c9cd3bda1bfab7cf13053cc052af852
Drew Schmidt <schmidt@math.utk.edu>
2016-01-10 12:40:47 -0500

    expand packages page slightly

---
+++ ui/js/buttons.js
@@ -0,0 +1,2 @@
+var rghb = function(){$("body").html($("body").html().replace(/ghbutton/g,'<span class="label label-default" style="background-color:green">GitHub</span>'));return;}; rghb();
+var rcb = function(){$("body").html($("body").html().replace(/cranbutton/g,'<span class="label label-default" style="background-color:blue">CRAN</span>'));return;}; rcb();
