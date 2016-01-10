7be8388032d824137c583c557319d4900656809d
Drew Schmidt <schmidt@math.utk.edu>
2016-01-10 14:27:08 -0500

    another try

--- ui/js/buttons.js
+++ ui/js/buttons.js
@@ -1,2 +1,3 @@
 var rghb = function(){$("body").html($("body").html().replace(/ghbutton/g,'<span class="label label-default" style="background-color:green">GitHub</span>'));return;}; rghb();
 var rcb = function(){$("body").html($("body").html().replace(/cranbutton/g,'<span class="label label-default" style="background-color:blue">CRAN</span>'));return;}; rcb();
+
