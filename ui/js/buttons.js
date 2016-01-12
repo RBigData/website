61b4bc7b72571d5040dc3f064276a77fbefd1cdb
George Ostrouchov <ostrouchovg@utk.edu>
2016-01-12 11:32:37 -0500

    pdf button

--- ui/js/buttons.js
+++ ui/js/buttons.js
@@ -1,5 +1,6 @@
 document.body.innerHTML = document.body.innerHTML.replace(/ghbutton/g, '<span class="label label-default" style="background-color:green">GitHub</span>');
 document.body.innerHTML = document.body.innerHTML.replace(/cranbutton/g, '<span class="label label-default" style="background-color:blue">CRAN</span>');
+document.body.innerHTML = document.body.innerHTML.replace(/pdfbutton/g, '<span class="label label-default" style="background-color:red">pdf</span>');
 
 var links = document.links;
 
