cdd95edd59d347e1a937bb237e132dbc31e9cd74
snoweye <wccsnow@gmail.com>
2016-01-12 05:31:54 -0500

    fix buttons.js and typo in packages.html

--- ui/js/buttons.js
+++ ui/js/buttons.js
@@ -1,3 +1,10 @@
 document.body.innerHTML = document.body.innerHTML.replace(/ghbutton/g, '<span class="label label-default" style="background-color:green">GitHub</span>');
 document.body.innerHTML = document.body.innerHTML.replace(/cranbutton/g, '<span class="label label-default" style="background-color:blue">CRAN</span>');
 
+var links = document.links;
+
+for (var i = 0, linksLength = links.length; i < linksLength; i++) {
+   if (links[i].hostname != window.location.hostname) {
+       links[i].target = '_blank';
+   } 
+}
