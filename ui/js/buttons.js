5d2582cf4a9727324ee148c3e4ca38ebbd925ce8
wrathematics <wrathematics@gmail.com>
2016-02-16 09:42:22 -0500

    color tweaks

--- ui/js/buttons.js
+++ ui/js/buttons.js
@@ -1,7 +1,7 @@
 document.body.innerHTML = document.body.innerHTML.replace(/ghbutton/g, '<span class="label label-default" style="background-color:green">GitHub</span>');
 document.body.innerHTML = document.body.innerHTML.replace(/cranbutton/g, '<span class="label label-default" style="background-color:blue">CRAN</span>');
-document.body.innerHTML = document.body.innerHTML.replace(/pdfbutton/g, '<span class="label label-default" style="background-color:red">pdf</span>');
-document.body.innerHTML = document.body.innerHTML.replace(/htmlbutton/g, '<span class="label label-default" style="background-color:#f97704">html</span>');
+document.body.innerHTML = document.body.innerHTML.replace(/pdfbutton/g, '<span class="label label-default" style="background-color:#c62326">pdf</span>');
+document.body.innerHTML = document.body.innerHTML.replace(/htmlbutton/g, '<span class="label label-default" style="background-color:#d36200">html</span>');
 
 var links = document.links;
 
